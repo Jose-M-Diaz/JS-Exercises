ade533b341b4fe0b1626ab2e53fe771b6bb27b3f
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-04-14 13:29:47 -0500

    had tip calc for final bill. corrected to tip only

--- JS/udemyJs.js
+++ JS/udemyJs.js
@@ -3,11 +3,11 @@
 //Write function called 'calcTip' that takes any bill value as an input and returns the corresponding tip: Tip 15% if bill between 50 and 300 and if different tip 20%
 const calcTip = (billValue) => {
   if (billValue >= 50 && billValue <= 300) {
-    let finalBill = billValue + billValue * 0.15;
-    return finalBill.toFixed(2);
+    let tip = billValue * 0.15;
+    return tip.toFixed(2);
   } else {
-    let finalBill = billValue + billValue * 0.2;
-    return finalBill.toFixed(2);
+    let tip = billValue * 0.2;
+    return tip.toFixed(2);
   }
 };
 console.log(calcTip(100));
@@ -15,3 +15,6 @@ console.log(calcTip(50));
 console.log(calcTip(300));
 console.log(calcTip(20));
 console.log(calcTip(11.45));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];

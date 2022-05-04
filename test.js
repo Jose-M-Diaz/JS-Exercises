1b14b75e3e2505b87a8c2b8e3df401872088ce64
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-05-04 15:44:26 -0500

    worked on css boxes.

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("Vlocity" && "Developer");
+
+var name = "vlocity";
+function foo() {
+  name = "developer";
+  return;
+  function name() {}
+}
+foo();
+console.log(foo());
+console.log(name);

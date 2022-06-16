bd047986fbc2948dfef67b0d1328a9ff2855a01a
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-06-16 11:26:42 -0500

    fizzbuzz completed

--- JS/fizzbuzz.js
+++ JS/fizzbuzz.js
@@ -1,14 +1,14 @@
 "use strict";
 
 let input = parseInt(prompt("Please enter a number for fizzBuzz."));
-for (let i = 1; i <= input; i++) {
+for (let i = 0; i <= input; i++) {
   // console.log(i);
-  if (i % 3 === 0 && i % 5 === 0) {
+  if (i % 15 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
   } else if (i % 3 === 0) {
     console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
   } else {
     console.log(i);
   }
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,9 @@
 "use strict";
+
+let num = 100;
+for (let i = 1; i <= num; i++) {
+  if (i % 15 === 0) console.log("FIZZBUZZ");
+  else if (i % 3 === 0) console.log("FIZZ");
+  else if (i % 5 === 0) console.log("BUZZ");
+  else console.log(i);
+}

104a1ca1c0fedf9ef02134167f32be147cbcbd0f
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-06-16 11:30:24 -0500

    added comments to fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,17 @@
 "use strict";
 
-let num = 100;
+let num = 100; // make variable the final number to reach
 for (let i = 1; i <= num; i++) {
-  if (i % 15 === 0) console.log("FIZZBUZZ");
-  else if (i % 3 === 0) console.log("FIZZ");
-  else if (i % 5 === 0) console.log("BUZZ");
-  else console.log(i);
+  // for loop to iterate through numbers starting from 1 and ending at final number
+  if (i % 15 === 0)
+    console.log(
+      "FIZZBUZZ"
+    ); // if iterated number is divisible by 15 show fizzbuzz. If is not
+  else if (i % 3 === 0)
+    console.log(
+      "FIZZ"
+    ); // try checking if divisible by 3 to show fizz. If is not
+  else if (i % 5 === 0)
+    console.log("BUZZ"); // try checking if divisible by 5 to show Buzz. If none
+  else console.log(i); // then just show the number iterated.
 }
